25ba81704919a8048118da3b540d2d754bd64376
leharagit <leharaoshan@icloud.com>
2025-03-10 19:36:51 +0530

    add new things

--- src/main/java/com/yourcompany/ecommerce/service/BidService.java
+++ src/main/java/com/yourcompany/ecommerce/service/BidService.java
@@ -1,16 +1,18 @@
 package com.yourcompany.ecommerce.service;
 
 import com.yourcompany.ecommerce.model.Bid;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface BidService {
     Bid createBid(Bid bid);
+    Optional<Bid> getMaxBidByProductId(String productId);
+
     Optional<Bid> getBidById(String bidId);
     List<Bid> getAllBids();
     Bid updateBid(String bidId, Bid bid);
     void deleteBid(String bidId);
     List<Bid> getBidsByProductId(String productId);
-    Optional<Bid> getMaxBidByProductId(String productId);
-    Optional<String> getMaxBidderPhoneNumber(String productId);
+    Optional<Bid> getMaxTotalAmountBid(); // Method to get max totalAmount bid
 }
